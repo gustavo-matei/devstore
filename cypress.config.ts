1d13331336d3d7c3fcab1b73e2c0c6a74bb8d39e
Gustavo Matei <gustavo.matei@barfacil.com.br>
2025-04-06 17:39:13 -0300

    chore: add client/server env variables boundary

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://192.168.16.119:3000',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
